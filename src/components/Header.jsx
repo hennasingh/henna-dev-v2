448da9b9e1d205da444e0cc99b921ba394bc35c3
Henna Singh <hennasingh04@gmail.com>
2025-05-14 08:08:30 +0100

    update vite scripts and folder structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+export default function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <img src="" alt="" />
+      </div>
+      <button className="nav-toggle" aria-label="toggle-navigation">
+        <span className="hamburger"></span>
+      </button>
+      <nav className="nav">
+        <ul className="nav__list">
+          <li className="nav__item">
+            <a href="#home" class="nav__link">
+              Home
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#services" class="nav__link">
+              My Services
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#about" class="nav__link">
+              About Me
+            </a>
+          </li>
+          <li className="nav__item">
+            <a href="#work" class="nav__link">
+              My Work
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}

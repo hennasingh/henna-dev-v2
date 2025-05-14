863c3be90c07168f4dc7688e52d37144b83a12bb
Henna Singh <hennasingh04@gmail.com>
2025-05-14 15:07:54 +0100

    update header spelling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Intro from "./components/Intro";
 import Services from "./components/Services";
 import AboutMe from "./components/AboutMe";

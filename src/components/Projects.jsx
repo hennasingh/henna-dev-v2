3a94facd9124f1f84b2caf07c5a772f04e896fbd
Henna Singh <hennasingh04@gmail.com>
2025-05-20 20:25:24 +0100

    setup supabase client and retrived projects in project component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../supabaseClient";
+import ProjectCard from "./ProjectCard";
+
+export default function Projects() {
+  const [projects, setProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const { data } = await supabase
+          .from("portfolio_projects")
+          .select("*")
+          .order("created_at", { ascending: false });
+        setProjects(data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    };
+
+    fetchProjects();
+  }, []);
+
+  if (loading) return <h2>Loading Projects....</h2>;
+
+  const projectList = projects?.map((project, idx) => {
+    return <ProjectCard key={idx} props={project} />;
+  });
+
+  return (
+    <section className="my-work" id="work">
+      <h2 className="section__title section__title--work">My Work</h2>
+      <p className="section__subtitle section__subtitle--work">
+        A selection of my projects
+      </p>
+    </section>
+  );
+}
